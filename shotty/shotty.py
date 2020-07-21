0c053875a8a4b39eb41b5dc3cc9e0fd665a065c0
iwsmith <iwsmith@nx-vi.local>
2020-07-20 21:08:56 -0400

    Adding stop/start/list

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -5,26 +5,73 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
 
-def list_instances():
+def filter_instances(project):
+    instances = []
+
+    if project:
+         filter = [{'Name':'tag:Project', 'Values':[project]}]
+         instances= ec2.instances.filter(Filters=filter)
+    else:
+        instances=ec2.instances.all()
+    return instances
+
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
+
+@click.option('--project', default=None, help="Only instances for the project (tag Project:<name>)") 
+
+def list_instances(project):
     """List instances"""
-    for i in ec2.instances.all():
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        tags={t['Key']: t['Value'] for t in i.tags or []}
         print(", ".join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            i.public_dns_name,
+            tags.get('Project', '<no project>')
+            )))
     return
 
-#nouns and verbs
-# instances snapshots
-# list
-# start / stop
-# tags
+@instances.command('stop')
+@click.option('--project', default=None,
+                help='Only instances for prokect')
+
+def stop_instances(project):
+    """Stop instances"""
+
+    instances = filter_instances(project)
+
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+    return
+
+@instances.command('start')
+@click.option('--project', default=None, help='Only instance for project')
+
+def start_instances(project):
+    """Start instances"""
+
+    instances = filter_instances(project)
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+    return
+
+
 
 
 if __name__ == '__main__':
-    list_instances()
+    instances()
 
